9717f8e8ef93b71e0cf0016488becc1e53870651
Elias Funes <sk8flip720@gmail.com>
2019-12-27 01:05:17 -0300

    First commit and version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Header from './components/Header';
+import Formulario from "./components/Formulario";
+import Error from "./components/Error";
+import Clima from "./components/Clima";
 
 function App() {
+
+  const [ciudad, guardarCiudad] = useState('');
+  const [pais, guardarPais] = useState('');
+  const [error, guardarError] = useState(false);
+  const [resultado, guardarResultado] = useState({})
+
+  useEffect(() => {
+    //prevenir ejecucion
+    if(ciudad === '') return;
+
+    const consultarApi = async () => {
+      const appId = '300a201e554e2839be2f79c1ab9dd913';
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      guardarResultado(resultado);
+    }
+
+    consultarApi();
+  }, [ciudad, pais])
+
+  const datosConsulta = datos => {
+    if(datos.ciudad === '' || datos.pais === ''){
+      guardarError(true);
+      return;
+    }
+
+    guardarCiudad(datos.ciudad);
+    guardarPais(datos.pais);
+    guardarError(false);
+  }
+
+
+
+  //cargar componente condicionalmente
+  let componente;
+  if(error) {
+    componente = <Error mensaje="Ambos campos son obligatorios"/>
+  } else if (resultado.cod === '404' ){
+    componente = <Error mensaje="La ciudad no existe en nuestro registro"/>
+  } else {
+    componente = <Clima resultado={resultado}/>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+     <Header
+         titulo='Clima React App'
+     />
+     <div className="contenedor-form">
+       <div className="container">
+         <div className="row">
+           <div className="col s12 m6">
+             <Formulario
+                datosConsulta={datosConsulta}
+             />
+           </div>
+           <div className="col s12 m6">
+             {componente}
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
   );
 }
 
---
+++ src/components/Clima.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Clima = ({resultado}) => {
+
+    const {name, main} = resultado;
+
+    //restar grados kelvin
+    const kelvin = 273.15;
+
+    if(!name) return null;
+
+    return (
+        <div className="card-panel white col s12">
+            <div className="black-text">
+                <h2>El clima de {name} es:</h2>
+                <p className="temperatura">{parseInt(main.temp - kelvin, 10)} <span>&#x2103;</span></p>
+                <p>Temperatura máxima : {parseInt(main.temp_max - kelvin, 10)} &#x2103;</p>
+                <p>Temperatura minima: {parseInt(main.temp_min  - kelvin, 10)} &#x2103;</p>
+            </div>
+        </div>
+    );
+};
+
+export default Clima;
